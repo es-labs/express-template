2b8f6b103473d75cc5feac8bee3a305e1d078949
ais-one <aaronjxz@gmail.com>
2026-04-21 20:06:16 +0800

    chore(repo): fix loading of env

--- common/compiled/node/config.ts
+++ common/compiled/node/config.ts
@@ -13,7 +13,6 @@ const envFilePath = path.resolve(process.cwd(), '.env');
 
 if (process.env.NODE_ENV === 'development') {
   // will throw if file doesn't exist, only use for development
-  loadEnvFile(`${envFilePath}.local`); // load this first
   loadEnvFile(envFilePath);
 }
 
